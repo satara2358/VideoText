33f4883d4729d2bd2765cea713e35d22a16d59ce
Satara <tatenokay@gmail.com>
2023-06-03 19:00:35 -0500

    CapturaVideoAText Youtube

--- videoText.py
+++ videoText.py
@@ -3,11 +3,9 @@ import pytube
 
 youtubeId = 'https://www.youtube.com/watch?v=JtcpAmOUshM&list=RDJtcpAmOUshM&start_radio=1'
 model = whisper.load_model('small')
-
 youtube = pytube.YouTube(youtubeId)
 audio = youtube.streams.get_audio_only()
 audio.download(filename='audio.mp4')
-
 result = model.transcribe('audio,mp4')
 
 print(result["text"])
